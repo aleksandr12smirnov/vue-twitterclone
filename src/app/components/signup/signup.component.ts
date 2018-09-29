39a8767c555e9b8401ae0911426347b02d758b4b
aleksandr12smirnov <alexandershni@gmail.com>
2018-09-29 10:55:23 -0500

    Error importing Joi on angular

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+// import Joi from 'joi';
+import * as Joi from 'joi'
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+
+  // Rules for the validation of inputs
+  schema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    username: Joi.string().regex(/(^[a-zA-Z0-9_]*$)/).min(8).max(20).required(),
+    password: Joi.string().trim().min(8).required()
+  });
+
 
   signupForm = new FormGroup({
     username: new FormControl('', Validators.required),
